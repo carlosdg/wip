0e9637daa5ae14d59690ebd90a055a2476dcf997
Carlos Domínguez García <carlosdg97@gmail.com>
2018-10-27 18:45:20 +0100

    Refactor: Histogram component
    - Moved the initialization of the visualization data out of the constructor to componentDidMount
    - Removed constructor

--- src/components/HistogramComponent/index.js
+++ src/components/HistogramComponent/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import 'react-vis/dist/style.css';
+import "react-vis/dist/style.css";
 import {
   FlexibleXYPlot,
   XAxis,
@@ -10,58 +10,52 @@ import {
   Hint
 } from "react-vis";
 
-
 class HistogramComponent extends Component {
+  state = {
+    emphasizedValue: null,
+    histogramVisualizationData: []
+  };
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      emphasizedValue: null,
-      histogram: props.histogram,
-      histogramVisualizationData: [],
-    };
-
-    this.state.histogram.histogramValues.forEach((value, index) => {
-      this.state.histogramVisualizationData.push({"x0": index, "x": index + 1, "y":value});
+  componentDidMount() {
+    this.setState({
+      histogramVisualizationData: this.props.histogram.histogramValues.map(
+        (value, index) => ({ x0: index, x: index + 1, y: value })
+      )
     });
   }
 
   render() {
-    //TODO: Remove width and height attributes to get a FlexibleXYPlot, the ScrollableContainer component
-    //which wraps Histogram component (in App.js) must be removed aswell.
     return (
       <FlexibleXYPlot
-        width={500}
-        height={500}
         margin={{
           top: 50,
           left: 70
         }}
-        onMouseLeave={() => this.setState({emphasizedValue: null})}
+        onMouseLeave={() => this.setState({ emphasizedValue: null })}
       >
         <VerticalGridLines />
         <HorizontalGridLines />
         <XAxis />
         <YAxis />
         <VerticalRectSeries
-          onNearestX={value => this.setState({emphasizedValue: value})}
+          onNearestX={value => this.setState({ emphasizedValue: value })}
           data={this.state.histogramVisualizationData}
         />
-        {
-          this.state.emphasizedValue ?
-            <Hint
-              value={this.state.emphasizedValue}
-              align={{horizontal: 'auto', vertical: 'top'}}
-              style={{fontSize: 14}}
-              format={dataPoint => {
-                return [{
-                  title:"Value: " + dataPoint.x0,
-                  value:"Count: " + dataPoint.y,
-                }];
-              }}
-            />
-            : null
-        }
+        {this.state.emphasizedValue ? (
+          <Hint
+            value={this.state.emphasizedValue}
+            align={{ horizontal: "auto", vertical: "top" }}
+            style={{ fontSize: 14 }}
+            format={dataPoint => {
+              return [
+                {
+                  title: "Value: " + dataPoint.x0,
+                  value: "Count: " + dataPoint.y
+                }
+              ];
+            }}
+          />
+        ) : null}
       </FlexibleXYPlot>
     );
   }
