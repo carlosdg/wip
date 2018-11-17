00d85cfe10086099e6eeb8a7bf7e17360c91ca18
Carlos Domínguez García <carlosdg97@gmail.com>
2018-11-17 16:53:22 +0000

    Fix: Decreased number of grid columns
    By default we allowed grid items to occupy 12 columns on large screens. Because items were too small we decreased it so each column is 1/10 instead.

--- src/lib/grid/gridProperties.js
+++ src/lib/grid/gridProperties.js
@@ -2,7 +2,7 @@
  * Object defining the constants properties of the Grids
  */
 export default {
-  GRID_COLUMNS: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
+  GRID_COLUMNS: { lg: 10, md: 8, sm: 6, xs: 4, xxs: 2 },
   LAYOUT_BREAKPOINTS: { lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 },
   DEFAULT_GRID_ITEM_COLS: 2, // Number of columns each item is tall on creation
   DEFAULT_GRID_ITEM_ROWS: 4, // Number of rows each item is wide on creation
