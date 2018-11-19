6b99dae1862a267bf4c58e3c1d92f2ad0d740d94
Carlos Domínguez García <carlosdg97@gmail.com>
2018-11-19 22:20:59 +0000

    Feat: Function to transform an image region
    Added a new function that accepts a region and applyies the lookup table only on that region to get a new image buffer with the same dimensions as the region

--- src/lib/ImageProcessing/transformImage.js
+++ src/lib/ImageProcessing/transformImage.js
@@ -7,11 +7,7 @@ import RgbaImageBuffer from "../RgbaImageBuffer";
  * @param {Array} lookupTable Lookup table
  * @returns {RgbaImageBuffer} Transformed image
  */
-export const transformImage = (
-  imgBuffer,
-  lookupTable
-) => {
-
+export const transformImage = (imgBuffer, lookupTable) => {
   let result = imgBuffer.copy();
   for (let i = 0; i < result.pixels.length; i += RgbaImageBuffer.NUM_CHANNELS) {
     let newValue = lookupTable[result.pixels[i]];
@@ -21,4 +17,29 @@ export const transformImage = (
   }
 
   return result;
-}
+};
+
+/** Returns an RgbaImageBuffer from the transformed region */
+export const transformImageRegion = (
+  imgBuffer,
+  lookupTable,
+  { top = 0, left = 0, height = imgBuffer.height, width = imgBuffer.width } = {}
+) => {
+  const resultPixels = new Uint8ClampedArray(
+    height * width * RgbaImageBuffer.NUM_CHANNELS
+  );
+  let resultIndex = 0;
+
+  for (let y = top; y < height; y += 1) {
+    for (let x = left; x < width; x += 1) {
+      const newValue = lookupTable[imgBuffer.getPixel({ x, y })[0]];
+      resultPixels[resultIndex] = newValue;
+      resultPixels[resultIndex + 1] = newValue;
+      resultPixels[resultIndex + 2] = newValue;
+      resultPixels[resultIndex + 3] = 255;
+      resultIndex += RgbaImageBuffer.NUM_CHANNELS;
+    }
+  }
+
+  return new RgbaImageBuffer(width, height, resultPixels);
+};
