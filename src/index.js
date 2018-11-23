c1115622e5abbb02ea285bc4af440d56b0b99cc0
Carlos Domínguez García <carlosdg97@gmail.com>
2018-11-23 22:56:49 +0000

    Style: Updated theme color

--- src/index.js
+++ src/index.js
@@ -12,17 +12,31 @@ import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 // Material UI color theme
 const theme = createMuiTheme({
   palette: {
+    common: { black: "#000", white: "#fff" },
+    background: { paper: "#fff", default: "#fafafa" },
     primary: {
-      light: "#d0e4f9",
-      main: "#9fb2c6",
-      dark: "#708395",
-      contrastText: "#000000"
+      light: "#7986cb",
+      main: "#3f51b5",
+      dark: "#303f9f",
+      contrastText: "#fff"
     },
     secondary: {
-      light: "#ffffff",
-      main: "#dddddd",
-      dark: "#ababab",
-      contrastText: "#000000"
+      light: "#ff4081",
+      main: "#f50057",
+      dark: "#c51162",
+      contrastText: "#fff"
+    },
+    error: {
+      light: "#e57373",
+      main: "#f44336",
+      dark: "#d32f2f",
+      contrastText: "#fff"
+    },
+    text: {
+      primary: "rgba(0, 0, 0, 0.87)",
+      secondary: "rgba(0, 0, 0, 0.54)",
+      disabled: "rgba(0, 0, 0, 0.38)",
+      hint: "rgba(0, 0, 0, 0.38)"
     }
   },
   typography: {
