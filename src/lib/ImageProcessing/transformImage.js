5d213e5e244879843fa8ffb161da840c128d70c9
Carlos Domínguez García <carlosdg97@gmail.com>
2018-11-23 21:34:26 +0000

    Refactor: Removed `transformImageRegion`
    It has been decided that we'll approach the regions in a different way, we'll just require the user to crop the image and then apply the operation

--- src/lib/ImageProcessing/transformImage.js
+++ src/lib/ImageProcessing/transformImage.js
@@ -18,28 +18,3 @@ export const transformImage = (imgBuffer, lookupTable) => {
 
   return result;
 };
-
-/** Returns an RgbaImageBuffer from the transformed region */
-export const transformImageRegion = (
-  imgBuffer,
-  lookupTable,
-  { top = 0, left = 0, height = imgBuffer.height, width = imgBuffer.width } = {}
-) => {
-  const resultPixels = new Uint8ClampedArray(
-    height * width * RgbaImageBuffer.NUM_CHANNELS
-  );
-  let resultIndex = 0;
-
-  for (let y = top; y < height; y += 1) {
-    for (let x = left; x < width; x += 1) {
-      const newValue = lookupTable[imgBuffer.getPixel({ x, y })[0]];
-      resultPixels[resultIndex] = newValue;
-      resultPixels[resultIndex + 1] = newValue;
-      resultPixels[resultIndex + 2] = newValue;
-      resultPixels[resultIndex + 3] = 255;
-      resultIndex += RgbaImageBuffer.NUM_CHANNELS;
-    }
-  }
-
-  return new RgbaImageBuffer(width, height, resultPixels);
-};
