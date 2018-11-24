0018f0ae7c444c78cc3a670fb4527657d41831b1
Carlos Domínguez García <carlosdg97@gmail.com>
2018-11-24 16:29:32 +0000

    Updates

---
+++ precache-manifest.04b356f4ee513eeec5f8fb1a3850c4a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d973a05cc57a516b434d",
+    "url": "/ImageProcessor/static/js/runtime~main.d973a05c.js"
+  },
+  {
+    "revision": "450a727cb674f71dec83",
+    "url": "/ImageProcessor/static/js/main.450a727c.chunk.js"
+  },
+  {
+    "revision": "892964a5d36b6d852d91",
+    "url": "/ImageProcessor/static/js/1.892964a5.chunk.js"
+  },
+  {
+    "revision": "450a727cb674f71dec83",
+    "url": "/ImageProcessor/static/css/main.a7d61524.chunk.css"
+  },
+  {
+    "revision": "892964a5d36b6d852d91",
+    "url": "/ImageProcessor/static/css/1.6cc8360e.chunk.css"
+  },
+  {
+    "revision": "dc8517fcbf5a6949e571bef8674416cf",
+    "url": "/ImageProcessor/index.html"
+  }
+];
