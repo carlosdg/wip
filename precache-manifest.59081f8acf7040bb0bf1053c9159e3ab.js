22daa3f07ebd50f97185e7193dfbad58c8163279
Carlos Domínguez García <carlosdg97@gmail.com>
2019-02-13 13:41:26 +0000

    Updates

---
+++ precache-manifest.59081f8acf7040bb0bf1053c9159e3ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd89a5d33e6430fad28c",
+    "url": "/wip/static/js/runtime~main.cd89a5d3.js"
+  },
+  {
+    "revision": "2926a249388ae4dc371d",
+    "url": "/wip/static/js/main.2926a249.chunk.js"
+  },
+  {
+    "revision": "75a2c0466f3254d2ef05",
+    "url": "/wip/static/js/2.75a2c046.chunk.js"
+  },
+  {
+    "revision": "2926a249388ae4dc371d",
+    "url": "/wip/static/css/main.e820e80f.chunk.css"
+  },
+  {
+    "revision": "75a2c0466f3254d2ef05",
+    "url": "/wip/static/css/2.431039c7.chunk.css"
+  },
+  {
+    "revision": "e1d17f1d504182129029cd4dee133d66",
+    "url": "/wip/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ImageProcessor/precache-manifest.ae9d81fc0cdc7eccd6629ad72db0a003.js"
+  "/wip/precache-manifest.59081f8acf7040bb0bf1053c9159e3ab.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ImageProcessor/index.html", {
+workbox.routing.registerNavigationRoute("/wip/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
